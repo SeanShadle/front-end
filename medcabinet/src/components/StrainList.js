82188742e2d413acde61e0bdf8e0cecb5c5e965b
Sean Shadle <seandshadle@gmail.com>
2020-09-23 15:37:42 -0500

    Added styling to strainlist

--- medcabinet/src/components/StrainList.js
+++ medcabinet/src/components/StrainList.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import strainData from '../mockData'
 import {useHistory} from 'react-router-dom'
 import { v4 as uuidv4 } from "uuid";
+import styled, {keyframes} from 'styled-components'
 
 const initialSearch = {
     search:"",
@@ -51,27 +52,71 @@ const StrainList = () => {
     
     return (
         <>
-            <input type="text" name="search" value={search.search} onChange={onChange} />
-            <select name="type" onChange={onSelectChange} >
-                    <option value='all'>All Items</option>
-                    <option value="indica">Indica</option>
-                    <option value="sativa">Sativa</option>
-                    <option value="hybrid">Hybrid</option>
-                </select>
+        <StrainMain>
+            <SearchBoxDiv>
+                <StrainInput type="text" name="search" placeholder="Strain Name" value={search.search} onChange={onChange} />
+                <StrainSelect name="type" onChange={onSelectChange} >
+                        <option value='all'>All Items</option>
+                        <option value="indica">Indica</option>
+                        <option value="sativa">Sativa</option>
+                        <option value="hybrid">Hybrid</option>
+                    </StrainSelect>
+            </SearchBoxDiv>
             <div className="items-list-wrapper">
                 {data
                 .filter((item)=>item.strain_name.toLowerCase().includes(makeLowerCase(search.search)))
                 .map((item)=>(
-                    <div className ="item-card" key={item.id} item={item} onClick={(ev) => routeToItem(ev, item)}>
-                        <img className="item-list-image" src={item.img} alt={item.strain_name} />
+                    <ItemCard className ="item-card" key={item.id} item={item} onClick={(ev) => routeToItem(ev, item)}>
+                        <ListImg className="item-list-image" src={item.img} alt={item.strain_name} />
                         <h4>{item.strain_name}</h4>
                         <h4>{item.type}</h4>
                         
-                    </div>
+                    </ItemCard>
                     
                 ))}
             </div>
+        </StrainMain>
         </>
     )
 }
+
+const StrainMain = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #F7F7F7;
+    width: 25vw;
+    margin-left: 37vw;
+    padding-top: 3vh;
+    border-radius: 10px;
+`
+
+const SearchBoxDiv = styled.div`
+    display:flex;
+    flex-direction: column;
+    width: 15vw;
+`
+
+const StrainSelect = styled.select`
+    padding: 1rem 12rem 1rem .5rem; 
+    margin-bottom: 1vh;
+    border-radius: 5px;
+`
+
+const StrainInput = styled.input` 
+    padding: 1rem 12rem 1rem .5rem; 
+    margin-bottom: 1vh;
+    border-radius: 5px;
+`
+
+const ItemCard = styled.div`
+    border-bottom: 1px dashed black;
+    padding: 3vh 0;
+`
+
+const ListImg = styled.img`
+    border-radius: 10px;
+`
+
+
 export default StrainList;
