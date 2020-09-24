bdb04f7b3c2818c5e6b696aaab5794111df49247
unknown <abdinajibmohamud@gmail.com>
2020-09-24 14:11:20 -0700

    changed layout of form to update, still bugs, but will revert back to original styling and format once it works

--- medcabinet/src/components/StrainForm.js
+++ medcabinet/src/components/StrainForm.js
@@ -1,85 +1,82 @@
-import React from 'react'
-import {useForm} from "react-hook-form"
-import styled, {keyframes} from 'styled-components'
+import {axiosWithAuth} from '../utils/axiosWithAuth'
+import React, { useState, useEffect } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
 
-export default function StrainForm(){
-    const {register, handleSubmit, errors} = useForm();
+const initialValues = {
+    strain_name: '',
+    type: '',
+    rating: '',
+    effects: '',
+    flavors: '',
+    description: ''
 
-    const onSubmit = data => {
-        console.log(data);
-    }
-
-    return (
-        <FormDiv>
-        <StyledForm onSubmit={handleSubmit(onSubmit)}>
-            <Styledh2>Product Search</Styledh2>
-            <b>Strain:</b>
-            <StyledInput type="text" placeholder="Strain" ref={register} />
-            <b>Effect:</b>
-            <StyledInput type="text" placeholder="Enter the desired effect" ref={register} />
-            <b>Flavor:</b>
-            <StyledInput type="text" placeholder="Enter the desired flavor" ref={register} />
-            <b>Description:</b>
-            <StyledInput type="text" placeholder="Description" ref={register}/>
-            <b>Type:</b>
-            <StyledSelect>
-                <option value="">Select a type</option>
-                <option value="Sativa">Sativa</option>
-                <option value="Indica">Inidica</option>
-                <option value="Hybrid">Hybrid</option>
-            </StyledSelect>
-            <b>Rating:</b>
-            <StyledSelect>
-                <option value="">Select a rating</option>
-                <option value="one">★☆☆☆☆</option>
-                <option value="two">★★☆☆☆</option>
-                <option value="three">★★★☆☆</option>
-                <option value="four">★★★★☆</option>
-                <option value="five">★★★★★</option>
-            </StyledSelect>
-            <StyledSubmit type="submit" value="Search" />
-        </StyledForm>
-        </FormDiv>
-    )
 }
 
-const FormDiv = styled.div`
-    display: flex;
-    background-color: #F7F7F7;
-    width: 25vw;
-    margin: 5vh 35vw;
-    padding: 3vh 0 5vh 5vw;
-    text-align: left;
-    font-family: 'Maven Pro', sans-serif;
-`
-const StyledForm = styled.form`
-    display:flex;
-    flex-direction: column;
-`
+const StrainForm = ({setList, list}) => {
+    const [values , setValues] = useState(initialValues)
+    const history = useHistory();
+    const {id} = useParams();
+    console.log(values)
+    useEffect(() => {
+        const itemy = list.find(
+            (thing) => `${thing.id}` === id);
+            setValues(itemy)
+       
+          
+      }, [id]);
+    
 
-const Styledh2 = styled.h2`
-    text-align:center;
-    font-size: 2rem;
-`
+    // const handleChanges = (event) => {
+    //     setValues({...values,[event.target.name] : event.target.value });
+    //   };
+    const handleChanges = (e) => {
+        e.persist();
+        let value = e.target.value;
+        if (e.target.name === 'effects' || 'flavors') {
+          value = value.split(',');
+        }
+        
+    
+        setValues({
+          ...values,
+          [e.target.name]: value,
+        });
+      };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axiosWithAuth()
+        .put(`/api/strain/edit/${id}`, values)
+        .then((res)=> {
+            setValues(res.data);
+            setList(list.map(item =>{
+                if(item.id== id){
+                    return values
+                } else{
+                    return item
+                }
+            }))
+            history.push(`/StrainList`);
+        }); 
+    };
 
-const StyledInput = styled.input` 
-    padding: 1rem 12rem 1rem .5rem; 
-    margin-bottom: 1vh;
-    border-radius: 5px;
-`
+    return(
+        <div className='update'>
+            <h2>Update A Strain</h2>
+            <div className='form'>
+                <form onSubmit={handleSubmit}>
+                    <input type= 'text' name='strain_name' value={values.strain_name} onChange={handleChanges} placeholder='Title'  />
+                    <input type='text' name ='type' value={values.type} onChange={handleChanges} placeholder='Director'  />
+                    <input type='number' name ='rating' value={values.rating} onChange={handleChanges} placeholder='MetaScore'  />
+                    <input type='stars' name ='effects' value={values.effects} onChange={handleChanges} placeholder=''  />
+                    <input type='stars' name ='flavors' value={values.flavors} onChange={handleChanges} placeholder=''  />
+                    <input type='stars' name ='description' value={values.description} onChange={handleChanges} placeholder=''  />
+                    <button>UPDATE</button> 
+                </form>
+                
+            </div>
+            
 
-const StyledSelect = styled.select`
-    padding: 1rem 12rem 1rem .5rem; 
-    margin-bottom: 1vh;
-    border-radius: 5px;
-`
-
-const StyledSubmit = styled.input`
-    width: 10vw;
-    color: white;
-    background-color: #044A53;
-    padding: 1rem 0rem;
-    text-align:center;
-    border-radius: 5px;
-    margin-left: 5vw
-`
+        </div>
+    )
+}
+export default StrainForm;
