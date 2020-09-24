6bc2946018dd50c3e5722223fa250d76d1930c45
Sean Shadle <seandshadle@gmail.com>
2020-09-24 16:00:12 -0500

    Fixed form validation on signup

--- medcabinet/src/components/Signup.js
+++ medcabinet/src/components/Signup.js
@@ -31,23 +31,29 @@ export default function Signup(){
         <StyledForm onSubmit={handleSubmit(onSubmit)}>
             <Styledh2>Med Cabinet</Styledh2>
             <h3><b>Signup</b></h3>
+            
             <b>First Name:</b>
-            <StyledInput type="text" placeholder="First Name" name="first-name" ref={register({required: "First name required"})} />
+            <StyledInput type="text" placeholder="First Name" name="first" ref={register({required: "First name required", minlength: {value: 1, message: "First Name required"}})} />
+            
             <b>Last Name:</b>
-            <StyledInput type="text" placeholder="Last Name" name="last-name" ref={register({required: "Last name required"})} />
+            <StyledInput type="text" placeholder="Last Name" name="last" ref={register({required: "Last name required", minlength: {value: 1, message: "Last Name required"}})} />
+            
             <b>Email:</b>
-            <StyledInput type="text" placeholder="Example@example.com" name="email" ref={register({required: "Email required"})} />
+            <StyledInput type="email" placeholder="Example@example.com" name="email" ref={register({required: "Email required"})} />
+            
             <b>Username:</b>
-            <StyledInput type="text" placeholder="Username" name="username" ref={register({required: "Username required", minLength: 4})} />
+            <StyledInput type="text" placeholder="Username" name="username" ref={register({required: "Username required", minlength: {value: 4, message: "Usernames must be atleast 4 characters long"}})} />
+            
             <b>Password:</b>
-            <StyledInput
-            type="password"
-            placeholder="Password"
-            name="password"
-            ref={register({required: "Password required", minLength: 8})}
-            />
+            <StyledInput type="password" placeholder="Password" name="password" ref={register({required: "Password required", minlength: {value: 8, message:"Passwords must be atleast 8 characters long"}})} />
+            
             <StyledP>We respect your privacy. Names and emails are not displayed publically. By creating your Med Cabinet account, you agree to the terms of use and privacy policy.</StyledP>
-        {errors.password && <p>{errors.password.message}</p>}
+            {errors.first && <StyledRequired>{errors.first.message}</StyledRequired>}
+            {errors.last && <StyledRequired>{errors.last.message}</StyledRequired>}
+            {errors.email && <StyledRequired>{errors.email.message}</StyledRequired>}
+            {errors.username && <StyledRequired>{errors.username.message}</StyledRequired>}
+            {errors.password && <StyledRequired>{errors.password.message}</StyledRequired>}
+            
             <StyledSubmit type="submit" value="Sign up"/>
             <LinkDiv>
                 <Link to ='/Login'>Already have an account? Login here!</Link>
@@ -98,5 +104,7 @@ const StyledSubmit = styled.input`
 const LinkDiv = styled.div`
     margin: 1vh 0 0 3.2vw;
 `
-
+const StyledRequired = styled.p`
+    color: red;
+`
 
